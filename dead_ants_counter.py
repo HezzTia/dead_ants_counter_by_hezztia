655654bf5085dae6baa5bde16d9d9da0a9c4f87a
HezzTia <emilycabreraarache@gmail.com>
2024-02-14 21:44:36 -0400

    added comments to code for aclarations

--- dead_ants_counter.py
+++ dead_ants_counter.py
@@ -1,19 +1,24 @@
-def count_dead_ants():
+#emily cabrera - 1106145
 
+def count_dead_ants():
+# this function asks the user for a line of text and counts the dead ants in it
     line = input("Enter a line of text: ")
 
     dead_ants = _count_dead_ants(line)
     return dead_ants
 
+# internal function to avoid duplication of code
 def _count_dead_ants(line):
     dead_ants = 0
     ant_count = line.count("ant")
 
+# counts the number of dead ants
     if ant_count > 0:
         bodies_count = line.count("nt") + line.count("tn")
         dead_ants = min(ant_count, bodies_count // 2)
 
     return dead_ants
 
+# prints the number of dead ants
 dead_ants = count_dead_ants()
 print(f"Number of dead ants: {dead_ants}")
